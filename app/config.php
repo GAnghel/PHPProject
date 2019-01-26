6088de76e18695e7dc35d8212864fec2b254e7e9
Gabriela Anghel <anghelgabrielaloredana97@gmail.com>
2019-01-26 11:11:28 +0200

    Modify config

--- app/config.php
+++ app/config.php
@@ -1,2 +1,15 @@
 <?php
-$config = ["env" => "dev"];
+namespace App;
+class Config{
+    const ENV = "dev";
+    const DB = [
+        "driver"  => "mysql",
+        'host'    => 'sandbox_db',
+        'dbname'  => 'sandbox',
+        'port'    => '3306',
+        'charset' => 'utf8mb4',
+        'user'    => 'root',
+        'pass'    => 'secret'
+    ];
+}
+//$config = ["env" => "dev"];
