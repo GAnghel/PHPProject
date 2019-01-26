28a122ea0b24bda55fd83d3f1ab3f377dbef0718
Gabriela Anghel <anghelgabrielaloredana97@gmail.com>
2019-01-26 11:13:24 +0200

    Modify router to work with  a guard

--- src/Router.php
+++ src/Router.php
@@ -1,23 +1,34 @@
 <?php
-
+namespace Framework;
 
 class Router {
-    public function __construct(){
-
+    protected $routes;
+    public function __construct($routes){
+        $this->routes = $routes;
     }
     public function handle($url, $queryStr){
-        require_once "../app/controllers/PageController.php";
+    
         if($this->match_uri($url)){
-            $controller = $routes[$url]["controller"];
-            $pageController = new $controller();
-            $pageController->{$routes[$url]["action"]}();
+            $controller = $this->routes[$url]["controller"];
+            $controllerName = "App\\controllers\\".$controller;
+            $this->checkGuard($url);
+            $pageController = new $controllerName();
+            $pageController->{$this->routes[$url]["action"]}();
         }
     }
 
     public function match_uri($url) : bool{
-            if(isset($routes[$url])){
+            if(isset($this->routes[$url])){
                 return true;
         }
        return false;
+    }
+
+    private function checkGuard(string $route): void{
+        if(isset($this->routes[$route]['guard'])) {
+            $guard = "\\App\\Guards\\".$this->routes[$route]['guard'];
+            //instantiate and execute the handle action
+            (new $guard)->handle();
+        }
     }
 }
