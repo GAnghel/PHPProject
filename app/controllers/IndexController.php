97388c03bf40abd478f551307f95687b77906da1
Gabriela Anghel <anghelgabrielaloredana97@gmail.com>
2019-01-27 10:56:36 +0200

    Add code for Login page. Couldn't configure Twig to load css file.

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -5,8 +5,6 @@ use \Framework\Utils\File;
 
 class IndexController extends \Framework\BaseController{
     public function indexAction(){
-        // $fileService = new File();
-        // $user = new \App\Models\User();
         return $this->view("user/index.html");
     }
 }
