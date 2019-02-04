a00962a9f1b49f00ce35740341bc65a16097dace
Gabriela Anghel <anghelgabrielaloredana97@gmail.com>
2019-02-04 21:45:26 +0200

    Add checks in guard

--- app/guards/Authenticated.php
+++ app/guards/Authenticated.php
@@ -1,15 +1,19 @@
 <?php
 namespace App\Guards;
-
 use Framework\Guard;
 
 class Authenticated implements Guard
 {
+    protected $connection;
     public function handle(array $params = null)
     {
         session_start();
-        if (!isset($_SESSION['username']))
+        if (!isset($_SESSION['uname'])) {
             $this->reject();
+        }
+        if (!isset($_SESSION['psw'])) {
+            $this->reject;
+        }
     }
 
     public function reject()
