8dd11a14e5cdd331e49539f5bf2987210f8f8ce1
Gabriela Anghel <anghelgabrielaloredana97@gmail.com>
2019-02-04 21:43:53 +0200

    Configure database connection parameters.

--- app/config.php
+++ app/config.php
@@ -4,12 +4,12 @@ class Config{
     const ENV = "dev";
     const DB = [
         "driver"  => "mysql",
-        'host'    => 'sandbox_db',
-        'dbname'  => 'sandbox',
+        'host'    => 'localhost',
+        'dbname'  => 'mydatabase',
         'port'    => '3306',
         'charset' => 'utf8mb4',
         'user'    => 'root',
-        'pass'    => 'secret'
+        'pass'    => ''
     ];
 }
 //$config = ["env" => "dev"];
