40d3d07ddfd05444c6eca87ac52bbbd40310934f
Gabriela Anghel <anghelgabrielaloredana97@gmail.com>
2019-02-04 21:44:13 +0200

    Add new routes

--- app/routes.php
+++ app/routes.php
@@ -1,7 +1,10 @@
 <?php
 $routes = [
             "/" => ["controller" => "IndexController", "action" => "indexAction"],
+            "/loginAction" => ["controller" => "LoginController", "action" => "loginAction"],
+            "/registerAction" => ["controller" => "RegisterController", "action" => "registerAction"],
+
             "post" => ["controller" => "PostController", "action" => "save"],
             "/page/about-us" => ["controller" => "PageController", "action" => "aboutUsAction"],
-            "/user/edit/{id}" => ["controller" => "UserController", "action" => "showAction", "guard" => "Authenticated"]
+            "/user/edit/{id}" => ["controller" => "UserController", "action" => "showAction", "guard" => "Authenticated"],
           ];
