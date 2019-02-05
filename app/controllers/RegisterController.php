5e916210213f9899435b651664d2033ebd8ee3e4
Gabriela Anghel <anghelgabrielaloredana97@gmail.com>
2019-02-05 10:37:20 +0200

    Modify RegisterController

--- app/controllers/RegisterController.php
+++ app/controllers/RegisterController.php
@@ -4,18 +4,6 @@ use Framework\BaseController as BaseController;
 
 class RegisterController extends BaseController{
     public function registerAction(){
-        $user = new \App\Models\User();
-        $fname = $_POST["fname"];
-        $lname = $_POST["lname"];
-        $email = $_POST["email"];
-        $username = $_POST["uname"];
-        $pass = $_POST["psw"];
-        $pass_repeat = $_POST["psw-repeat"];
-        if($pass === $pass_repeat){
-            if($user->register($fname, $lname, $username, $email, $pass) === true){
-                return $this->view("register/register.html");
-            }
-        }
-            
+        return $this->view("register/register.html");            
     }
 }
