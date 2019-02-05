b396a3ce04c83afad60f72b0daeca9f8e4ae0379
Gabriela Anghel <anghelgabrielaloredana97@gmail.com>
2019-02-05 10:36:53 +0200

    Add suplimentary routes

--- app/routes.php
+++ app/routes.php
@@ -3,8 +3,8 @@ $routes = [
             "/" => ["controller" => "IndexController", "action" => "indexAction"],
             "/loginAction" => ["controller" => "LoginController", "action" => "loginAction"],
             "/registerAction" => ["controller" => "RegisterController", "action" => "registerAction"],
-
-            "post" => ["controller" => "PostController", "action" => "save"],
+            "/logout" =>["controller" => "LogoutController", "action" => "logoutAction"],
+            "/save" => ["controller" => "PostController", "action" => "save"],
             "/page/about-us" => ["controller" => "PageController", "action" => "aboutUsAction"],
             "/user/edit/{id}" => ["controller" => "UserController", "action" => "showAction", "guard" => "Authenticated"],
           ];
