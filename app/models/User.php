a3579b73691373ffacd8aa69fdf7ed0613cd3a6e
Gabriela Anghel <anghelgabrielaloredana97@gmail.com>
2019-02-05 10:37:32 +0200

    Modify register action

--- app/models/User.php
+++ app/models/User.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Models;
 use Framework\Model;
 
@@ -8,28 +7,23 @@ class User extends Model
     protected $table = 'credentials';
     function validate(string $username, string $pass) : bool
     {
-        $result = $this->executeStatement("SELECT * FROM $this->table WHERE Username = ?", [$username]);
+        $result = $this->find([$username]);
         $isPassOk = false;
         if($pass == $result->Password){
             $isPassOk = true;
         }
         return $isPassOk;
     }
-    function register( string $fname, string $lname, string $uname, string $email,string $pass) : bool{
-        $credentialsResult = $this->executeStatement("INSERT INTO $this->table VALUES", [$uname], [$email], [$pass]);
-        $lastInsertedID = $credentialsResult->CredentialsID;
+    function register( string $fname, string $lname, string $uname, string $email, string $pass) : bool{
+        $credentialsID = $this->new(["Username"=>$uname, "E-mail"=>$email, "Password"=>$pass]);
         $table = 'user';
-        if($lastInsertedID > 0){
-            $userResult = $this->executeStatement("INSERT INTO $this->table VALUES", [$fname], [$lname], [$lastInsertedID], [1]);
+        $active = 1;
+        if($credentialsID > 0){
+            $userResult = $this->new(["FirstName"=>$fname, "LastName"=>$lname, "CredentialsID"=>$credentialsID, "active"=>$active]);
         } else {
             return false;
         }
         return true;
     }
-    function sessionStarter($result)
-    {
-        session_start();
-        $_SESSION["username"] = $result["User"]; //se salveaza in session, va fi disponibil mereu in aplicatie
-        echo "Private page";
-    }
+    
 }
